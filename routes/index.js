6ea69d5287d2f63d4bd0456b03d242a20536ea88
Klaw26 <batbat471@gmail.com>
2024-04-30 18:07:25 +0200

    Création de l'index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var jsonDb = require('../projects.json');
+// jsonDb = JSON.stringify(jsonDb);
+console.log(jsonDb);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project Viewer' });
+  res.render('index', { title: 'Project Viewer' , json: jsonDb});
 });
 
 module.exports = router;
